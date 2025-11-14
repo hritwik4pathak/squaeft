67d9aae7614a1bbdbb05a8f18556a21bdb3d70fa
hritwik4pathak <hritwik4pathak@gmail.com>
2025-11-14 16:28:50 +0530

    updated conection

--- src/app/api/webhooks/clerk/rout.js
+++ src/app/api/webhooks/clerk/rout.js
@@ -14,7 +14,7 @@ export async function POST(req) {
 
 //   create new Svix instance with secret
 
-  const wh = new Webhook(WEBHOOK_SECRET);
+const wh = new Webhook(SIGNING_SECRET);
 // Get headers
   const headerPayload = await headers();
   const svix_id= headerPayload.get('svix-id');
