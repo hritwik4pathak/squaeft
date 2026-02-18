5edb3b5711091c3b55c23056dbcf7ab6166eed9a
hritwik4pathak <hritwik4pathak@gmail.com>
2026-02-18 07:19:25 +0530

    added create listing page with some updates in package modules

--- src/app/create-listing/page.jsx
+++ src/app/create-listing/page.jsx
@@ -1,4 +1,3 @@
-import react from 'react';
 export default function CreateListing() {
       return(
         <main className='p-3 max-w-4xl mx-auto'>
@@ -16,13 +15,133 @@ export default function CreateListing() {
                        minLength='10'
                        required
                        />
-                       <textarea 
+                    <textarea 
+                       type='text'
+                       placeholder='Description'
+                       className='border p-3 rounded-lg'
+                       id='description'
+                       required
+                          />
+                    <input
                           type='text'
-                          placeholder='Description'
+                          placeholder='Price'
                           className='border p-3 rounded-lg'
-                          id='description'
+                          id='price'
                           required
                           />
+                <div className='flex flex-wrap gap-6'>
+                    <div className='gap-2 flex'>
+                         <input
+                                   type='checkbox'
+                                   id='sale'
+                                   className='w-5'
+                                   />
+                                   <span>sell</span>
+                    </div>
+                    <div className='gap-2 flex'>
+                                <input
+                                   type='checkbox'  
+                                      id='rent'
+                                      className='w-5'
+                                      />
+                                        <span>rent</span>
+                    </div>
+                    <div className='gap-2 flex'>
+                                <input
+                                   type='checkbox'
+                                        id='Parking'
+                                        className='w-5'
+                                        />
+                                        <span>Parking Spot</span>
+                    </div>
+                    <div className='gap-2 flex'>
+                                <input
+                                   type='checkbox'
+                                        id='Furnished'
+                                        className='w-5'
+                                        />  
+                                        <span>Furnished</span>
+                    </div>
+                    <div className='gap-2 flex'>
+                                <input
+                                   type='checkbox'  
+                                        id='offer'
+                                        className='w-5'
+                                        />  
+                                        <span>Offer</span>
+                    </div>
+                    </div>
+                    <div className='flex-wrap flex gap-6'>
+                    <div className='gap-2 items-center flex'>
+                                <input
+                                   type='number'
+                                        id='bedrooms'
+                                        min={1}
+                                        max={10}
+                                        required
+                                        className=' p-2 border-gray-400 border rounded-lg'
+                                        />
+                                        <p>Beds</p>
+                    </div>
+                    <div className='gap-2 items-center flex'>
+                                <input
+                                        type='number'
+                                        id='bathrooms'
+                                        min={1}
+                                        max={10}
+                                        required
+                                        className=' p-2 border-gray-400 border rounded-lg'
+                                        />
+                                        <p>Baths</p>
+                    </div>
+                    <div className='gap-2 items-center flex'>
+                                <input  
+                                      type='number'
+                                        id='RegularPrice'
+                                        min={100}
+                                        max={10000}
+                                        required
+                                        className=' p-2 border-gray-400 border rounded-lg'
+                                        />
+                                        <p>Regular Price</p>
+
+                     </div>
+                    <div className='gap-2 items-center flex'>
+                                 <input  
+                                        type='number'
+                                        id='DiscountedPrice'
+                                        min={100}
+                                        max={10000}
+                                        className=' p-2 border-gray-400 border rounded-lg'
+                                        />
+                                        <p>Discounted Price</p>
+                                        <span className="text-xs">(Rs. / month)</span>
+                    </div>
+                </div>
+              </div>
+                <div className='flex flex-col flex-1 gap-4'>
+                    <p className='text-lg font-semibold'>
+                        Images:
+                        <span className="font-normal text-sm ml-2 text-gray-400">
+                            The first image will be the cover image (Max 6)
+                            </span>
+                            </p>
+                            <div className='flex gap-4'>
+                               <input
+                                  type='file'
+                                  id='images'
+                                    accept='.jpg,.png,.jpeg'
+                                    multiple
+                                    required
+                                    className='border p-3 rounded-lg w-full'
+                                    />
+                            <button className='border border-black text-green-700 p-3 rounded-lg'>
+                                Upload
+                            </button>
+                            </div>
+                            <button className='bg-slate-700 text-white p-3 rounded-lg'>
+                                CREATE LISTING
+                            </button>    
                 </div>
             </form>
         </main>
